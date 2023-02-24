d6f2252bb3d123310bc23c738724e0f4ecca4f8f
Shane Hurtado <mhurtado1@berklee.edu>
2023-02-24 15:10:08 -0600

    final

---
+++ main.js
@@ -0,0 +1,16 @@
+// Put your code here
+
+const names = [
+   "Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+];
+
+console.log("All Pace Names");
+
+for (const name of names) {
+  console.log(name);
+}
+
+const filteredNames = names.filter(name => name.startsWith("The"));
+
+console.log(filteredNames)
